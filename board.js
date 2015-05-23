aa67a166b5eb17b8704519f40b9487d243940ac7
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-05-22 20:28:37 -0700

    adding player dragging

--- board.js
+++ board.js
@@ -31,6 +31,9 @@ function Player(){
 
   //create the enemies (in an array) - enemy data
   var enemies = [];
+  var player = [];
+
+
   //generate N enemies in a loop - new Enemy() and push it into the array
   for(var id = 0; id < Game.numberOfEnemies; id++){
     enemies.push( new Enemy(id) );
@@ -42,22 +45,46 @@ function Player(){
     .attr("width", Game.width)
     .attr("height", Game.height);
 
-    board
-      .selectAll('circle')
-      .data(enemies)
-      .enter()
-      .append('circle')
-      .attr("cx", function(enemy){
-        return enemy.x;
-      })
-      .attr("cy", function(enemy){
-        return enemy.y;
-      })
-      .attr("r", Game.enemyRadius)
-      .style("fill", "purple");
+  //create enemies
+  board
+    .selectAll('circle')
+    .data(enemies)
+    .enter()
+    .append('circle')
+    .attr("cx", function(enemy){
+      return enemy.x;
+    })
+    .attr("cy", function(enemy){
+      return enemy.y;
+    })
+    .attr("r", Game.enemyRadius)
+    .style("fill", "purple");
+
+
+  //drag feature
+  var drag = d3.behavior.drag()
+    .on("drag", dragmove);
+
+  function dragmove(d) {
+    //console.log("drag event fired:", d, d3.event.x, d3.event.y);
+
+    d3.select(this)
+    .attr("cx", d3.event.x)
+    .attr("cy", d3.event.y);
+    //d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
+  }
+  //create player
+  board
+    .append('ellipse')
+    .attr("cx", Game.width /2)
+    .attr("cy", Game.height /2)
+    .attr("rx", Game.enemyRadius /2)
+    .attr("ry", Game.enemyRadius /2)
+    .style("fill", "orange")
+    .call(drag);
 
   //setInterval callback function
-  function loop(){
+  function animationLoop(){
     //loop through each enemy. tell it to update its x and y
     enemies.forEach(function(enemy){
       enemy.move();
@@ -67,15 +94,21 @@ function Player(){
     board
       .selectAll('circle')
       .data(enemies)
-      .attr("cx", function(enemy){
+      .transition().duration(1000)
+       .attr("cx", function(enemy){
         return enemy.x;
-      })
-      .attr("cy", function(enemy){
+        })
+        .attr("cy", function(enemy){
         return enemy.y;
       });
   }
 
-  loop();
-  setInterval(loop, 500);
+  setInterval(animationLoop, 1000);
+
+  //collision detection loop
+  setInterval(function(){
+
+  }, 500);
+
 })();
 
